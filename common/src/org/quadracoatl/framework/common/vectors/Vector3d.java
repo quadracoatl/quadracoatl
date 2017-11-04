088239dff438c730e5ea9f3fcaf8f1519be6f7ba
Robert Zenz <Robert.Zenz@bonsaimind.org>
2017-11-04 20:45:17 +0100

    Extended Vector3d functionality.

--- common/src/org/quadracoatl/framework/common/vectors/Vector3d.java
+++ common/src/org/quadracoatl/framework/common/vectors/Vector3d.java
@@ -38,6 +38,73 @@ public class Vector3d {
 		this.z = z;
 	}
 	
+	public static final Vector3d center(Vector3d pointA, Vector3d pointB, Vector3d pointC) {
+		return center(pointA, pointB, pointC, new Vector3d());
+	}
+	
+	public static final Vector3d center(Vector3d pointA, Vector3d pointB, Vector3d pointC, Vector3d target) {
+		target.x = (pointA.x + pointB.x + pointC.x) / 3.0d;
+		target.y = (pointA.y + pointB.y + pointC.y) / 3.0d;
+		target.z = (pointA.z + pointB.z + pointC.z) / 3.0d;
+		
+		return target;
+	}
+	
+	public static final double dot(Vector3d vectorA, Vector3d vectorB) {
+		return (vectorA.x * vectorB.x)
+				+ (vectorA.y * vectorB.y)
+				+ (vectorA.z * vectorB.z);
+	}
+	
+	public static final Vector3d normal(Vector3d pointA, Vector3d pointB, Vector3d pointC) {
+		return normal(pointA, pointB, pointC, new Vector3d());
+	}
+	
+	public static final Vector3d normal(Vector3d pointA, Vector3d pointB, Vector3d pointC, Vector3d target) {
+		double vx = pointB.x - pointA.x;
+		double vy = pointB.y - pointA.y;
+		double vz = pointB.z - pointA.z;
+		
+		double wx = pointC.x - pointA.x;
+		double wy = pointC.y - pointA.y;
+		double wz = pointC.z - pointA.z;
+		
+		target.x = (vy * wz) - (vz * wy);
+		target.y = (vz * wx) - (vx * wz);
+		target.z = (vx * wy) - (vy * wx);
+		target.normalize();
+		
+		return target;
+	}
+	
+	public void add(double value) {
+		add(value, value, value);
+	}
+	
+	public void add(double x, double y, double z) {
+		this.x = this.x + x;
+		this.y = this.y + y;
+		this.z = this.z + z;
+	}
+	
+	public void add(Vector3d vector) {
+		add(vector.x, vector.y, vector.z);
+	}
+	
+	public void divide(double value) {
+		divide(value, value, value);
+	}
+	
+	public void divide(double x, double y, double z) {
+		this.x = this.x / x;
+		this.y = this.y / y;
+		this.z = this.z / z;
+	}
+	
+	public void divide(Vector3d vector) {
+		divide(vector.x, vector.y, vector.z);
+	}
+	
 	public boolean equals(double x, double y, double z) {
 		return Double.doubleToLongBits(this.x) == Double.doubleToLongBits(x)
 				&& Double.doubleToLongBits(this.y) == Double.doubleToLongBits(y)
@@ -68,6 +135,10 @@ public class Vector3d {
 		return true;
 	}
 	
+	public double getLength() {
+		return Math.sqrt((x * x) + (y * y) + (z * z));
+	}
+	
 	public double getX() {
 		return x;
 	}
@@ -94,6 +165,54 @@ public class Vector3d {
 		return result;
 	}
 	
+	public void multiply(double value) {
+		multiply(value, value, value);
+	}
+	
+	public void multiply(double x, double y, double z) {
+		this.x = this.x * x;
+		this.y = this.y * y;
+		this.z = this.z * z;
+	}
+	
+	public void multiply(Vector3d vector) {
+		multiply(vector.x, vector.y, vector.z);
+	}
+	
+	public void normalize() {
+		double length = getLength();
+		
+		if (length != 0.0d) {
+			x = x / length;
+			y = y / length;
+			z = z / length;
+		}
+	}
+	
+	public void rotate(double radiansX, double radiansY, double radiansZ) {
+		// TODO Rotation around X is missing.
+		
+		double length = getLength();
+		double theta = Math.acos(y / length);
+		double phi = Math.atan2(z, x);
+		
+		if (phi >= 0.0d) {
+			theta = theta + radiansZ;
+		} else {
+			theta = theta - radiansZ;
+		}
+		
+		phi = phi + radiansY;
+		
+		x = length * Math.sin(theta) * Math.cos(phi);
+		y = length * Math.cos(theta);
+		z = length * Math.sin(theta) * Math.sin(phi);
+	}
+	
+	public void rotate(Vector3d vector) {
+		rotate(vector.x, vector.y, vector.z);
+	}
+	
 	public void set(double x, double y, double z) {
 		this.x = x;
 		this.y = y;
@@ -116,6 +235,20 @@ public class Vector3d {
 	
 	public void setZ(double z) {
 		this.z = z;
+	}
+	
+	public void subtract(double value) {
+		subtract(value, value, value);
+	}
+	
+	public void subtract(double x, double y, double z) {
+		this.x = this.x - x;
+		this.y = this.y - y;
+		this.z = this.z - z;
+	}
+	
+	public void subtract(Vector3d vector) {
+		subtract(vector.x, vector.y, vector.z);
 	}
 	
 	@Override
